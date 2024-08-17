f55aad2b8f26731b39c5f3bea6f3c083a8749bfc
Vaibhav252525 <keniv935@gmail.com>
2024-08-18 00:10:26 +0530

    initial commit

---
+++ Video 65/Script.js
@@ -0,0 +1,19 @@
+// function factorial(n){
+//     let fact=1;
+//     let n=5;
+//     for(let i=1;i<=n;i++){
+//         fact= fact*i;
+//     }
+//     return fact;
+
+// }
+
+
+// console.log(facFor(fac));
+
+let person={
+    name:"vaibhav",
+    age:"22",
+    gender:"male"
+};
+console.log(person.name);
