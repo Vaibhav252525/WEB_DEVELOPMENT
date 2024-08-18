f73c8088b787476338f872d263eee33b166919ea
Vaibhav252525 <keniv935@gmail.com>
2024-08-18 14:16:50 +0530

    initial commit

---
+++ Video 68/Script.js
@@ -0,0 +1,9 @@
+let heading=document.getElementById("head");
+console.log(heading);
+
+let paragraph= document.getElementsByClassName("para");
+console.dir(paragraph);
+
+let tag= document.getElementsByTagName("p")
+console.dir(tag);
+console.log(tag);
