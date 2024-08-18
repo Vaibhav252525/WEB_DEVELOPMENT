55c8462ede6c38d2cce86d967cb21508a2073f52
Vaibhav252525 <keniv935@gmail.com>
2024-08-18 12:44:40 +0530

    initial commit

---
+++ Video 63/Script.js
@@ -0,0 +1,38 @@
+// let colors=["red","green","blue"];
+// console.log(colors[0]);
+// colors[1]="white";
+// console.log(colors.length);
+
+// for(var i=0;i<colors.length;i++){
+//     var result=colors[i];
+//     console.log(colors[i]);
+// }
+// colors.push("black")
+// console.log(colors);
+// colors.pop();
+// console.log(colors)
+// console.log(colors.toString())
+
+
+// let marks=[10,20,30,40,50];     //Finding average marks
+// let sum=0;
+// for(let val of marks){
+//     sum+=val;
+    
+// }
+// console.log(sum);
+// let avg= sum/marks.length;
+// console.log(avg);
+
+
+//concatinating two arrays
+
+let array=["potato","onion","garlic"];
+let array1=["bmw","oooo","mercedez"];
+console.log(array.concat(array1));
+array.unshift("bhindi");
+console.log(array);
+array.shift(array[0]);
+console.log(array);
+
+console.log(window.document);
